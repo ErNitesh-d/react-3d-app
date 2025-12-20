9c327eb6071592ad7d0f1868bdceae2485d11da4
ErNitesh-d <your_email@gmail.com>
2025-12-20 16:13:03 +0530

    modified cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -23,7 +23,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[550px] w-full'
+        className='bg-tertiary p-5 rounded-2xl sm:w-[520px] w-full'
       >
         <div className='relative w-full h-[400px] bg-white rounded-2xl overflow-hidden'>
           <img
