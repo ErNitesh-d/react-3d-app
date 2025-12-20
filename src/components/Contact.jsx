0f186e7a2d0e4990b9e971e4a3585b4ccc685d5d
ErNitesh-d <your_email@gmail.com>
2025-12-20 15:29:52 +0530

    final commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,17 +31,16 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-     emailjs
-      .send(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      emailjs
+      .send('service_un6j3u8','template_c80iayg',
         {
           from_name: form.name,
           to_name: "Nitesh Dagiya",
           from_email: form.email,
-          to_email: "emaiml@gmail.com",
+          to_email: "nitinkhatri9898@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'flJuzjY5xwhnRcAVk'
       )
       .then(
         () => {
